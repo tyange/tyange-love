12132ee99ba70ab313a40f0135827749af4af37d
tyange <usun16@gmail.com>
2026-06-28 14:38:52 +0900

    메타데이터 수정

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,5 +1,5 @@
 /** 사이트 전역 메타데이터. OG/canonical 등 절대 URL 생성에 사용. */
 export const SITE_URL = 'https://love.tyange.com';
-export const SITE_NAME = 'lovelog';
-export const SITE_DESCRIPTION = '읽고 쓰는 개인 블로그, lovelog';
+export const SITE_NAME = 'love.tyagne.com';
+export const SITE_DESCRIPTION = 'lovelog';
 export const OG_IMAGE = `${SITE_URL}/og-image.png`;
