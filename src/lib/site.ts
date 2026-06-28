372c7f7c7958a577560c3e41e39d86d27026daa3
tyange <usun16@gmail.com>
2026-06-28 15:29:47 +0900

    edited site name

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,4 +1,4 @@
 export const SITE_URL = 'https://love.tyange.com';
-export const SITE_NAME = 'love.tyagne.com';
+export const SITE_NAME = 'love.tyange.com';
 export const SITE_DESCRIPTION = 'lovelog';
 export const OG_IMAGE = `${SITE_URL}/og-image.png`;
